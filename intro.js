f16a66275cb5ed6c90ed7202ea8e9c0b6f06d21f
JesseMambu12 <jessechristianmambu@gmail.com>
2025-01-23 10:43:48 +0700

    catatan intro.js

--- intro.js
+++ intro.js
@@ -1,2 +1,12 @@
 console.log('Intro to Git')
 var fullName = 'Jesse';
+
+
+
+// git add .
+// git commit -m "message" = untuk mensave atau mengcom,ot
+// git log --online = untuk liat history commit
+// git reset --soft kodeonline = untuk meng undo dari commit dari activity yg ada di kodeonline
+// git reset --soft kodeonline = untuk meng reset dari commit dari activity yg ada di kodeonline
+// git remote add origin linkGithubRepo  = untuk me connect Git dengan repisitory dari Github (jgn lupa buat repisitory baru di Github)
+// git push origin master = 
