593158a70b11b5ef1219339a0b969c4213061ad8
JesseMambu12 <jessechristianmambu@gmail.com>
2025-01-23 11:14:34 +0700

    Create login.js

--- intro.js
+++ intro.js
@@ -9,6 +9,6 @@ var fullName = 'Jesse';
 // git reset --soft kodeonline = untuk meng undo dari commit dari activity yg ada di kodeonline
 // git reset --soft kodeonline = untuk meng reset dari commit dari activity yg ada di kodeonline
 // git remote add origin linkGithubRepo  = untuk me connect Git dengan repisitory dari Github (jgn lupa buat repisitory baru di Github)
-// git push origin master = 
+// git push origin master = untuk menyimpan perubahan pada git ke github
 
 // git checkout -b branch_name = untuk membuat branch baru
